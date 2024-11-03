9c7cb631634cfa7f32163146d4d177a3b2e95afd
derefff <deref122@gmail.com>
2024-11-03 20:36:13 +0100

    clang-format all files

--- src/compiler/lexer.c
+++ src/compiler/lexer.c
@@ -1,25 +1,24 @@
-#include <ctype.h>
 #include "lexer.h"
+#include "tokens.h"
+#include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "tokens.h"
 
-#define DEBUG_CURSOR_CHAR(lexer) \
-    printf("cursor:%d \t char:\t %c\t ascii_form: %d \n", (lexer)->cursor, (lexer)->current_char, (lexer)->current_char)
+#define DEBUG_CURSOR_CHAR(lexer) printf("cursor:%d \t char:\t %c\t ascii_form: %d \n", (lexer)->cursor, (lexer)->current_char, (lexer)->current_char)
 
-void advance_lexer(Lexer *lexer)
+void advance_lexer(Lexer* lexer)
 {
-  if(lexer->content_len > lexer->cursor)
+  if (lexer->content_len > lexer->cursor)
   {
     lexer->cursor++;
     lexer->current_char = lexer->content[lexer->cursor];
   }
 }
 
-Lexer* init_lexer(const char *content, long content_len)
+Lexer* init_lexer(const char* content, long content_len)
 {
-  Lexer *lexer = malloc(sizeof(Lexer));
+  Lexer* lexer = malloc(sizeof(Lexer));
   lexer->cursor = 0;
   lexer->content = content;
   lexer->content_len = content_len;
@@ -28,7 +27,7 @@ Lexer* init_lexer(const char *content, long content_len)
 
 Token* create_token(token_t token_type, char* value)
 {
-  Token *token = malloc(sizeof(Token));
+  Token* token = malloc(sizeof(Token));
   token->type = token_type;
   token->val = value;
   return token;
@@ -37,27 +36,24 @@ Token* create_token(token_t token_type, char* value)
 // handling whitespaces and new line symbol '\n'
 void skip_whitespaces(Lexer* lexer)
 {
-  while(lexer->current_char == ' ' || lexer->current_char == '\t' || lexer->current_char == '\n')
+  while (lexer->current_char == ' ' || lexer->current_char == '\t' || lexer->current_char == '\n')
   {
-    if(lexer->current_char == ' ' || lexer->current_char == '\t') advance_lexer(lexer);
-    if(lexer->current_char == '\n') advance_lexer(lexer);
+    if (lexer->current_char == ' ' || lexer->current_char == '\t')
+      advance_lexer(lexer);
+    if (lexer->current_char == '\n')
+      advance_lexer(lexer);
   }
 }
 
 // look at current_character + 1, don't move cursor
-char peek(Lexer* lexer)
-{
-  return lexer->content[lexer->cursor+1];
-}
+char peek(Lexer* lexer) { return lexer->content[lexer->cursor + 1]; }
 
-char peek_by(Lexer* lexer, int offset)
-{
-  return lexer->content[lexer->cursor+offset];
-}
+char peek_by(Lexer* lexer, int offset) { return lexer->content[lexer->cursor + offset]; }
 
 void skip_by_lexer(Lexer* lexer, int offset)
 {
-  if(lexer->content_len < lexer->cursor+offset) lexer->cursor+=offset;
+  if (lexer->content_len < lexer->cursor + offset)
+    lexer->cursor += offset;
 }
 
 char* get_identifier(Lexer* lexer)
@@ -66,15 +62,17 @@ char* get_identifier(Lexer* lexer)
   int identifier_name_length = 1;
   char* identifier_name = malloc(sizeof(char) * identifier_name_length);
 
-  //while(lexer->current_char != ' ' && lexer->current_char != '\0' && lexer->current_char != '\t' || lexer->current_char != '\n')
-  while(isalnum(lexer->current_char) || lexer->current_char == '_')
+  // while(lexer->current_char != ' ' && lexer->current_char != '\0' &&
+  // lexer->current_char != '\t' || lexer->current_char != '\n')
+  while (isalnum(lexer->current_char) || lexer->current_char == '_')
   {
-    if(lexer->cursor % identifier_name_length == 0)
+    if (lexer->cursor % identifier_name_length == 0)
     {
-     // printf("cursor: %d\t ident_len: %d\t mod: %d\n", lexer->cursor, identifier_name_length,lexer->cursor % identifier_name_length );
+      // printf("cursor: %d\t ident_len: %d\t mod: %d\n", lexer->cursor,
+      // identifier_name_length,lexer->cursor % identifier_name_length );
       identifier_name = realloc(identifier_name, identifier_name_length * sizeof(char));
     }
-    identifier_name[identifier_name_length-1] = lexer->current_char;
+    identifier_name[identifier_name_length - 1] = lexer->current_char;
     identifier_name_length++;
     // DEBUG_CURSOR_CHAR(lexer);
     advance_lexer(lexer);
@@ -88,9 +86,9 @@ char* get_string_literal(Lexer* lexer)
 {
   char c = lexer->current_char;
   int literal_size = 1;
-  char* string_literal = malloc(sizeof(char)*literal_size);
+  char* string_literal = malloc(sizeof(char) * literal_size);
 
-  while(lexer->current_char != '"')
+  while (lexer->current_char != '"')
   {
     // printf("\t literal_string_current_char: %c \n", lexer->current_char);
     string_literal = realloc(string_literal, literal_size * sizeof(char));
@@ -110,162 +108,160 @@ Token* get_next_token(Lexer* lexer)
 
   skip_whitespaces(lexer);
 
-
-  if (lexer->cursor >= lexer->content_len) {
-      return create_token(TOKEN_EOF, "\0");
+  if (lexer->cursor >= lexer->content_len)
+  {
+    return create_token(TOKEN_EOF, "\0");
   }
 
-
-  if(isalpha(lexer->current_char) && !isdigit(lexer->current_char))
+  if (isalpha(lexer->current_char) && !isdigit(lexer->current_char))
   {
     // printf("potential identifier %s \n", get_identifier(lexer));
 
     return temp_token = create_token(TOKEN_ID, "");
-
   }
 
-  switch(lexer->current_char)
+  switch (lexer->current_char)
   {
-    case '"': //TODO 'lorem ipsum' would be also a string literal and ... char I guess
+  case '"': // TODO 'lorem ipsum' would be also a string literal and ... char I guess
+    advance_lexer(lexer);
+    temp_token = create_token(TOKEN_STRING_LITERAL, get_string_literal(lexer));
+    return temp_token;
+    break;
+
+  case '+':
+    if (peek(lexer) == '+' && peek_by(lexer, 2) == '+')
+    {
+      temp_token = create_token(TOKEN_ADD, "");
       advance_lexer(lexer);
-      temp_token = create_token(TOKEN_STRING_LITERAL, get_string_literal(lexer));
-      return temp_token;
-    break;
-
-    case '+':
-      if(peek(lexer) == '+' && peek_by(lexer, 2) == '+' )
-      {
-        temp_token = create_token(TOKEN_ADD, "");
-        advance_lexer(lexer);
-      }
-      else if(peek(lexer) == '+' && peek_by(lexer, 2) != '+' )
-      {
-        temp_token = create_token(TOKEN_INCREMENT, "");
-        advance_lexer(lexer);
-        advance_lexer(lexer);
-      }
-      else
-      {
-        temp_token = create_token(TOKEN_ADD, "");
-        advance_lexer(lexer);
-      }
-
-      return temp_token;
-    break;
-
-    case '-':
-      if(peek(lexer) == '-' && peek_by(lexer, 2) == '-' )
-      {
-        temp_token = create_token(TOKEN_SUBTRACT, "");
-        advance_lexer(lexer);
-      }
-      else if(peek(lexer) == '-' && peek_by(lexer, 2) != '-' )
-      {
-        temp_token = create_token(TOKEN_DECREMENT, "");
-        advance_lexer(lexer);
-        advance_lexer(lexer);
-      }
-      else
-      {
-        temp_token = create_token(TOKEN_SUBTRACT, "");
-        advance_lexer(lexer);
-      }
-    break;
-
-    case '*':
-      temp_token = create_token(TOKEN_MULTIPLY, "");
+    }
+    else if (peek(lexer) == '+' && peek_by(lexer, 2) != '+')
+    {
+      temp_token = create_token(TOKEN_INCREMENT, "");
       advance_lexer(lexer);
-      return temp_token;
-    break;
-
-    case '/':
-      temp_token = create_token(TOKEN_DIVIDE, "");
       advance_lexer(lexer);
-      return temp_token;
-    break;
-
-    case '=':
-      if(peek(lexer) != '=')
-      {
-        temp_token = create_token(TOKEN_ASSIGN, "");
-        advance_lexer(lexer);
-      }
-
-      if(peek(lexer) == '=')
-      {
-        temp_token = create_token(TOKEN_EQ, "");
-        advance_lexer(lexer);
-        advance_lexer(lexer);
-      }
-
-      return temp_token;
-    break;
-
-    case '(':
-      temp_token = create_token(TOKEN_OPEN_PARANTHESIS, "");
+    }
+    else
+    {
+      temp_token = create_token(TOKEN_ADD, "");
       advance_lexer(lexer);
-      return temp_token;
+    }
+
+    return temp_token;
     break;
 
-    case ')':
-      temp_token = create_token(TOKEN_CLOSE_PARANTHESIS, "");
+  case '-':
+    if (peek(lexer) == '-' && peek_by(lexer, 2) == '-')
+    {
+      temp_token = create_token(TOKEN_SUBTRACT, "");
       advance_lexer(lexer);
-      return temp_token;
+    }
+    else if (peek(lexer) == '-' && peek_by(lexer, 2) != '-')
+    {
+      temp_token = create_token(TOKEN_DECREMENT, "");
+      advance_lexer(lexer);
+      advance_lexer(lexer);
+    }
+    else
+    {
+      temp_token = create_token(TOKEN_SUBTRACT, "");
+      advance_lexer(lexer);
+    }
     break;
 
-    case '{':
-      temp_token = create_token(TOKEN_OPEN_BRACE, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case '*':
+    temp_token = create_token(TOKEN_MULTIPLY, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    case '}':
-      temp_token = create_token(TOKEN_CLOSE_BRACE, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case '/':
+    temp_token = create_token(TOKEN_DIVIDE, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    case '[':
-      temp_token = create_token(TOKEN_OPEN_BRACKET, "");
+  case '=':
+    if (peek(lexer) != '=')
+    {
+      temp_token = create_token(TOKEN_ASSIGN, "");
       advance_lexer(lexer);
-      return temp_token;
+    }
+
+    if (peek(lexer) == '=')
+    {
+      temp_token = create_token(TOKEN_EQ, "");
+      advance_lexer(lexer);
+      advance_lexer(lexer);
+    }
+
+    return temp_token;
     break;
 
-    case ']':
-      temp_token = create_token(TOKEN_CLOSE_BRACKET, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case '(':
+    temp_token = create_token(TOKEN_OPEN_PARANTHESIS, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    case ';':
-      temp_token = create_token(TOKEN_SEMICOLON, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case ')':
+    temp_token = create_token(TOKEN_CLOSE_PARANTHESIS, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    case ':':
-      temp_token = create_token(TOKEN_COLON, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case '{':
+    temp_token = create_token(TOKEN_OPEN_BRACE, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    case '.':
-      temp_token = create_token(TOKEN_PERIOD, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case '}':
+    temp_token = create_token(TOKEN_CLOSE_BRACE, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    case ',':
-      temp_token = create_token(TOKEN_COMMA, "");
-      advance_lexer(lexer);
-      return temp_token;
+  case '[':
+    temp_token = create_token(TOKEN_OPEN_BRACKET, "");
+    advance_lexer(lexer);
+    return temp_token;
     break;
 
-    default:
-      DEBUG_CURSOR_CHAR(lexer);
-      temp_token =  create_token(TOKEN_INVALID, "Invalid Token at "+lexer->cursor);
-      advance_lexer(lexer);
-      return temp_token;
+  case ']':
+    temp_token = create_token(TOKEN_CLOSE_BRACKET, "");
+    advance_lexer(lexer);
+    return temp_token;
+    break;
+
+  case ';':
+    temp_token = create_token(TOKEN_SEMICOLON, "");
+    advance_lexer(lexer);
+    return temp_token;
+    break;
+
+  case ':':
+    temp_token = create_token(TOKEN_COLON, "");
+    advance_lexer(lexer);
+    return temp_token;
+    break;
+
+  case '.':
+    temp_token = create_token(TOKEN_PERIOD, "");
+    advance_lexer(lexer);
+    return temp_token;
+    break;
+
+  case ',':
+    temp_token = create_token(TOKEN_COMMA, "");
+    advance_lexer(lexer);
+    return temp_token;
+    break;
+
+  default:
+    DEBUG_CURSOR_CHAR(lexer);
+    temp_token = create_token(TOKEN_INVALID, "Invalid Token at " + lexer->cursor);
+    advance_lexer(lexer);
+    return temp_token;
     break;
   }
 }
--- src/main.c
+++ src/main.c
@@ -1,47 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include<assert.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "compiler/tokens.h"
 #include "compiler/lexer.h"
+#include "compiler/tokens.h"
 
-int main(int argc, char **argv )
+int main(int argc, char** argv)
 {
-  FILE *file;
-  if(argc > 1){
+  FILE* file;
+  if (argc > 1)
+  {
     file = fopen(argv[1], "r");
-    if(!file)
+    if (!file)
     {
-      fprintf(stderr,"File doesn't exist!\n");
+      fprintf(stderr, "File doesn't exist!\n");
       return 1;
     }
   }
   else
   {
-    fprintf(stderr,"No argument was passed!\n");
+    fprintf(stderr, "No argument was passed!\n");
     return 2;
   }
 
-  fseek(file,0, SEEK_END);
+  fseek(file, 0, SEEK_END);
   long file_size = ftell(file);
   rewind(file);
 
-  //size_t fread ( void * ptr, size_t size, size_t count, FILE * stream );
+  // size_t fread ( void * ptr, size_t size, size_t count, FILE * stream );
   char* file_content;
-  file_content = (char*) malloc (sizeof(char)*file_size);
+  file_content = (char*)malloc(sizeof(char) * file_size);
   fread(file_content, 1, file_size, file);
   file_content[file_size] = '\0';
 
   printf("filesize: %ld \n", file_size);
- // for(long i = 0; i < file_size; i++) printf("%c", file_content[i]);
+  // for(long i = 0; i < file_size; i++) printf("%c", file_content[i]);
 
   Lexer* lex = init_lexer(file_content, file_size);
 
   Token* program_tokens = malloc(sizeof(Token));
   Token* token = malloc(sizeof(Token));
 
-  while((token = get_next_token(lex))->type != TOKEN_EOF)
+  while ((token = get_next_token(lex))->type != TOKEN_EOF)
   {
     free(token);
   }
