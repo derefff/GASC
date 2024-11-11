4bd27216fe31c9d4139001b56fe414dd8e616aa8
derefff <deref122@gmail.com>
2024-11-11 11:29:02 +0100

    parser: initialization and skeleton of a file

--- src/compiler/parser.c
+++ src/compiler/parser.c
@@ -1,2 +1,15 @@
 #include "parser.h"
+#include "lexer.h"
 
+Parser* init_parser(Lexer* lexer)
+{
+  Parser* parser = (Parser*)malloc(sizeof(Parser));
+  parser->lexer = lexer;
+  parser->current_token = get_next_token(parser->lexer);
+  return parser;
+}
+
+void advance_parser(Parser* parser)
+{
+  parser->current_token = get_next_token(parser->lexer);
+}
--- src/compiler/parser.h
+++ src/compiler/parser.h
@@ -1,4 +1,23 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "AST.h"
+#include "lexer.h"
+#include "tokens.h"
+
+typedef struct
+{
+  Lexer* lexer;
+  Token* current_token;
+} Parser;
+
+Parser* init_parser(Lexer* lexer);
+ASTNode* parse_program(Parser* parser);
+ASTNode* parse_statement(Parser* parser);
+ASTNode* parse_expression(Parser* parser);
+ASTNode* parse_term(Parser* parser);
+void advance_parser(Parser* parser);
+void expect_token(Parser* parser, token_t token_type);
+void free_ast(ASTNode* node);
+
 #endif
